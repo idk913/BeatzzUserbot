c7d04496ce7583686c85ca93466f07abf2a317c3
idk913 <68418175+idk913@users.noreply.github.com>
2020-07-22 20:51:37 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -4,13 +4,13 @@ from uniborg.util import admin_cmd
 from userbot import ALIVE_NAME
 from telethon.tl.types import ChannelParticipantsAdmins
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
-PM_IMG = "https://telegra.ph/file/4f70284cc53a3de49369d.png"
+PM_IMG = "https://telegra.ph/file/9bb8404210db4414a479f.png"
 pm_caption = "`BEATZZ IS:` **ONLINE**\n\n"
 pm_caption += "**SYSTEM STATUS**\n"
 pm_caption += "`TELETHON VERSION:` **6.0.9**\n`Python:` **3.8.5**\n"
 pm_caption += "`DATABASE STATUS:` **Functional**\n"
 pm_caption += "**Current Branch** : `master`\n"
-pm_caption += "**USERBOT DEV** : {@BeatsToHell913}\n"
+pm_caption += "**USERBOT DEV** : @BeatsToHell913\n"
 pm_caption += f"**My Boss** : {DEFAULTUSER} \n"
 pm_caption += "**Heroku Database** : `AWS - Working Properly`\n\n"
 #@command(outgoing=True, pattern="^.alive$")
