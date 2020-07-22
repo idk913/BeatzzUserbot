54f4b86217fa169df0d5c073d412df42836e7e9d
idk913 <68418175+idk913@users.noreply.github.com>
2020-07-22 17:29:04 +0530

    Update autopic.py

--- userbot/plugins/autopic.py
+++ userbot/plugins/autopic.py
@@ -23,7 +23,6 @@ async def autopic(event):
     while True:
         shutil.copy(downloaded_file_name, photo)
         im = Image.open(photo)
-        file_test = im.rotate(counter, expand=False).save(photo, "PNG")
         current_time = datetime.now().strftime("%H:%M")
         img = Image.open(photo)
         drawn_text = ImageDraw.Draw(img)
