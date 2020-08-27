81d6a759c99c8a7d42d1637d4619a71e81a2cc1f
idk913 <68418175+idk913@users.noreply.github.com>
2020-08-27 15:25:46 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -19,7 +19,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             rev_text = query[::-1]
             buttons = paginate_help(0, CMD_LIST, "helpme")
             result = builder.article(
-                "© FridayUserBot Help",
+                "© BeatzzUserBot Help",
                 text="{}\nTotal Plugins Loaded: {}".format(
                     query, len(CMD_LIST)),
                 buttons=buttons,
@@ -38,7 +38,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = "Kindly Don't Use My Userbot ! \nGet Your Own Userbot. To Learn Ib @StarkxD"
+            reply_pop_up_alert = "Kindly Don't Use My Userbot ! \nGet Your Own Userbot. To Learn from @BeatsToHell913 xD"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
 
@@ -57,7 +57,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = "Get your own friday userbot, don't use Mine\n ib @StarkxD for learning how to get userbot!"
+            reply_pop_up_alert = "Get your own B.E.A.T.Z.Z userbot, don't use Mine\n Don't disturb @BeatsToHell913 for learning how to get userbot!"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
     @tgbot.on(events.callbackquery.CallbackQuery(  # pylint:disable=E0602
         data=re.compile(b"us_plugin_(.*)")
