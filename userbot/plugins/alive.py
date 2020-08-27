13ff06886a54c5f2af6954e18c7dee78462c280a
idk913 <68418175+idk913@users.noreply.github.com>
2020-08-27 16:02:50 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -4,10 +4,12 @@ from uniborg.util import admin_cmd
 from userbot import ALIVE_NAME
 from telethon.tl.types import ChannelParticipantsAdmins
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
-PM_IMG = "https://telegra.ph/file/9bb8404210db4414a479f.png"
-pm_caption = "`BEATZZ IS:` **ONLINE**\n\n"
+PM_IMG = "https://telegra.ph/file/17fb2a6feda681c95e478.png"
+pm_caption = "`BEATZZ IS:` **ONLINE & Up-to-date**\n\n"
 pm_caption += "**SYSTEM STATUS**\n"
 pm_caption += "`TELETHON VERSION:` **6.0.9**\n`Python:` **3.8.5**\n"
+pm_caption += "`IoT Device:` **Active, Connected to Cloud**\n"
+pm_caption += "`Protocol:` **MQTT, Secure**\n"
 pm_caption += "`DATABASE STATUS:` **Functional**\n"
 pm_caption += "**Current Branch** : `master`\n"
 pm_caption += "**USERBOT DEV** : @BeatsToHell913\n"
