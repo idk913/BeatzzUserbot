6484d349d9e324cd2404d199f127725cb02b52ba
idk913 <68418175+idk913@users.noreply.github.com>
2020-09-01 20:13:06 +0530

    Update getmusic.py

--- userbot/plugins/getmusic.py
+++ userbot/plugins/getmusic.py
@@ -12,17 +12,17 @@ async def _(event):
     if event.fwd_from:
         return
     name = event.pattern_match.group(1)
-    chat = "@SpotifyMusicDownloaderBot"
+    chat = "@DeezerMusicBot"
     await event.edit("```B.E.A.T.Z.Z is Getting Your Music```")
     async with bot.conversation(chat) as conv:
           await asyncio.sleep(2)
-          await event.edit("`Downloading Music \nIt may take some time\n   So Stay Tuned.....`")
+          await event.edit("`B.E.A.T.Z.Z is Downloading Music \nIt may take some time\n   So Stay Tuned.....`")
           try:
-              response = conv.wait_event(events.NewMessage(incoming=True,from_users=752979930))
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=595898211))
               await bot.send_message(chat, name)
               respond = await response
           except YouBlockedUserError:
-              await event.reply("```Please unblock @SpotifyMusicDownloaderBot and try again```")
+              await event.reply("```Please unblock @DeezerMusicBot and try again```")
               return
           await event.delete()
           await bot.forward_messages(event.chat_id, respond.message)
